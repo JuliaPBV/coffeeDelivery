92c5f57aa1e268ff25c36605bb6d23d9f6cc3f91
Julia Pereira <vital.pereira.julia@gmail.com>
2025-02-03 11:06:41 -0300

    personalizando a StatusBar

--- coffeeDelivery/App.tsx
+++ coffeeDelivery/App.tsx
@@ -1,4 +1,4 @@
-import { Text, View } from "react-native";
+import { Text, View, StatusBar } from "react-native";
 import {
   useFonts,
   Roboto_400Regular,
@@ -9,7 +9,19 @@ export default function App() {
   const [fonstLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
 
   return (
-    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+    <View
+      style={{
+        flex: 1,
+        alignItems: "center",
+        justifyContent: "center",
+        backgroundColor: "#8047F8",
+      }}
+    >
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
       {fonstLoaded ? <Text>Hello World!</Text> : <View />}
     </View>
   );
